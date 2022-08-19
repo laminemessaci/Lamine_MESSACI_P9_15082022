db79271a3f28a563134762bbd2b82e8af78f7c6d
laminemessaci <lamine.messaci18@gmail.com>
2022-08-19 13:06:44 +0200

    Added somes comment for tests

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
@@ -42,4 +42,66 @@ describe("Given I am connected as an employee", () => {
       expect(mailIcon.className).toBe("active-icon");
     });
   });
+
+  describe("when I submit the form with empty fields", () => {
+    test("then I should stay on new 'Bill page'", () => {
+      // Navigate to the new page
+      window.onNavigate(ROUTES_PATH.NewBill);
+      // Create instant of newBill
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        mockStore,
+        localStorage: window.localStorage,
+      });
+
+      expect(screen.getByTestId("expense-name").value).toBe("");
+      expect(screen.getByTestId("datepicker").value).toBe("");
+      expect(screen.getByTestId("amount").value).toBe("");
+      expect(screen.getByTestId("vat").value).toBe("");
+      expect(screen.getByTestId("pct").value).toBe("");
+      expect(screen.getByTestId("file").value).toBe("");
+
+      const form = screen.getByTestId("form-new-bill");
+      // mock the function handleSubmit
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(form).toBeTruthy();
+    });
+  });
+
+   describe("when I upload a file with the wrong format", () => {
+     test("then it should return an error message", async () => {
+       document.body.innerHTML = NewBillUI();
+       const onNavigate = (pathname) => {
+         document.body.innerHTML = ROUTES({ pathname });
+       };
+      // Create instant of newBill 
+       const newBill = new NewBill({
+         document,
+         onNavigate,
+         mockStore,
+         localStorage: window.localStorage,
+       });
+      // Create file with the unauthorized format 
+       const file = new File(["hello"], "hello.txt", { type: "document/txt" });
+       
+       const inputFile = screen.getByTestId("file");
+       // mock for handleChangeFile function 
+       const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+       inputFile.addEventListener("change", handleChangeFile);
+
+       fireEvent.change(inputFile, { target: { files: [file] } });
+
+       expect(handleChangeFile).toHaveBeenCalled();
+       expect(inputFile.files[0].type).toBe("document/txt");
+       await waitFor(() => screen.getByTestId("file-error-message"));
+       expect(screen.getByTestId("file-error-message").classList).not.toContain(
+         "hidden"
+       );
+     });
+   });
 });
